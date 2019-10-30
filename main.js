621fa682814eeaeb97687dc5e567fbaf64087ca4
Jardel Felipe Knirsch <jardelfelipeknirsch@gmail.com>
2019-10-29 22:50:43 -0300

    Testes

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
-const Gpio = require('onoff').Gpio;
-const sensorLuminosidade = new Gpio(4, 'in', '');
-const button = new Gpio(17, 'in', 'both');
+// const Gpio = require('onoff').Gpio;
 const mcpadc = require('mcp-spi-adc');
 
-const tempSensor = mcpadc.open(18, { speedHz: 20000 }, err => {
+const tempSensor = mcpadc.open(5, { speedHz: 20000 }, err => {
+  console.log(`Erro ao iniciar -> ${JSON.stringify(err)}`);
   if (err) throw err;
 
   setInterval(_ => {
     tempSensor.read((err, reading) => {
+      console.log(`Erro ao ler -> ${JSON.stringify(err)}`);
       if (err) throw err;
 
       console.log((reading.value * 3.3 - 0.5) * 100);
@@ -15,25 +15,6 @@ const tempSensor = mcpadc.open(18, { speedHz: 20000 }, err => {
   }, 1000);
 });
 
-button.watch((error, value) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(value);
-  }
-});
-
-sensorLuminosidade.watch((error, value) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(value);
-  }
-});
-
-unexportOnClose = () => {
-  sensorLuminosidade.unexport();
-  button.unexport();
-};
+unexportOnClose = () => {};
 
 process.on('SIGINT', unexportOnClose); //function to run when user closes using ctrl+c
